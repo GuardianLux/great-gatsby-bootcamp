8d85501c9656c8ba63c727a9b693894c3aa07a27
Matt Hendrie <matthendrie@pop-os.localdomain>
2019-06-13 10:51:48 +1000

    removed link

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ const IndexPage = () => {
             <h2>
                 Welcome to my site, where I'll be posting information about projects I'm working on in my freetime, as well as my experiences completing my Masters
             </h2>
-            <p>Need to get in touch? <Link to="/contact">Contact Me.</Link></p>
         </Layout>
     )
 }
